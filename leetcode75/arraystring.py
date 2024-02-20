4f11f83929772a60d201e3da46b3dafc9cb1be12
Nicholas Quezada <git.sharper162@passmail.net>
2024-02-19 23:07:55 -0700

    Add new problem

--- leetcode75/arraystring.py
+++ leetcode75/arraystring.py
@@ -1,11 +1,57 @@
-def merge_strings_alternatively(one: str, two: str) -> str:
-    result = ""
-    count = 0
-    while count < len(one) or count < len(two):
-        if count < len(one):
-            result += one[count]
-        if count < len(two):
-            result += two[count]
-        count += 1
+from typing import List
 
-    return result
+
+def greatest_common_divisor(x, y):
+    if y == 0:
+        return x
+    return greatest_common_divisor(y, x % y)
+
+
+class ArrayStringSolution:
+    # https://leetcode.com/problems/merge-strings-alternately
+    def mergeAlternatively(self, one: str, two: str) -> str:
+        result = ""
+        count = 0
+        while count < len(one) or count < len(two):
+            if count < len(one):
+                result += one[count]
+            if count < len(two):
+                result += two[count]
+            count += 1
+
+        return result
+
+    # https://leetcode.com/problems/greatest-common-divisor-of-strings
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        gcd = greatest_common_divisor(len(str1), len(str2))
+
+        return str1[:gcd]
+
+    # https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+    def kidsWithCandies(self, candies: List[int], extra_candies: int) -> List[bool]:
+        greatest = max(candies)
+        return [x + extra_candies >= greatest for x in candies]
+
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        prev_is_empty = True
+        count = 0
+        for i in range(len(flowerbed)):
+            middle_plot = flowerbed[i]
+            if middle_plot == 0:
+                empty_left_plot = prev_is_empty or i == 0
+                empty_right_plot = i == len(flowerbed) - 1 or flowerbed[i + 1] == 0
+
+                if empty_left_plot and empty_right_plot:
+                    count += 1
+                    prev_is_empty = True
+                elif middle_plot == 0:
+                    prev_is_empty = True
+            else:
+                prev_is_empty = False
+            if count >= n:
+                return True
+        return False
--- tests/arraystring_test.py
+++ tests/arraystring_test.py
@@ -1,7 +1,9 @@
 import unittest
-import time
+
 import leetcode75.arraystring as arraystring
 from util import time_function
+
+asSolution = arraystring.ArrayStringSolution()
 
 
 class ArrayStringTest(unittest.TestCase):
@@ -15,8 +17,42 @@ class ArrayStringTest(unittest.TestCase):
             ('', 'b', 'b'),
         ]
         for one, two, expected in test_cases:
-            got = time_function(arraystring.merge_strings_alternatively, one, two)
-            self.assertEqual(expected, got)
+            actual = time_function(asSolution.mergeAlternatively, one, two)
+            self.assertEqual(expected, actual)
+
+    def test_gcd_of_strings(self):
+        test_cases = [
+            ("abcabc", "abc", "abc"),
+            ("abab", "ab", "ab"),
+            ("leet", "code", ""),
+        ]
+        for one, two, expected in test_cases:
+            actual = time_function(asSolution.gcdOfStrings, one, two)
+            self.assertEqual(expected, actual)
+
+    def test_kids_with_candies(self):
+        test_cases = [
+            ([2, 3, 5, 1, 3], 3, [True, True, True, False, True]),
+            ([4, 2, 1, 1, 2], 1, [True, False, False, False, False]),
+            ([12, 1, 12], 10, [True, False, True]),
+        ]
+        for one, two, expected in test_cases:
+            actual = time_function(asSolution.kidsWithCandies, one, two)
+            self.assertEqual(expected, actual)
+
+    def test_can_place_flowers(self):
+        test_cases = [
+            ([1, 0, 0, 0, 1], 1, True),
+            ([1, 0, 0, 0, 1], 2, False),
+            ([0], 1, True),
+            ([0], 2, False),
+            ([0, 1, 0], 1, False),
+            ([0, 0, 0], 1, True),
+            ([0, 0, 0], 2, True),
+        ]
+        for flowerBed, toPlant, expected in test_cases:
+            actual = time_function(asSolution.canPlaceFlowers, flowerBed, toPlant)
+            self.assertEqual(expected, actual, f'flowerBed={flowerBed}, toPlant={toPlant}')
 
 
 if __name__ == '__main__':
